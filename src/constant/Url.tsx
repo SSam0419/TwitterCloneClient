7784cc87f010fc52248ee3a734e9326edb73a010
Sam Lee <leemingchun419@gmail.com>
2023-09-01 15:21:14 +0800

    cors

--- src/constant/Url.tsx
+++ src/constant/Url.tsx
@@ -1,3 +1,4 @@
-export const DOMAIN = "https://git.heroku.com/twitter-clone-api-demo.git/";
+export const DOMAIN =
+  "https://twitter-clone-api-demo-b7dad0d09f28.herokuapp.com/";
 // export const DOMAIN = "https://localhost:44385/";
 // export const DOMAIN = "http://localhost:7021/";
