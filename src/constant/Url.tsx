a87a4b34f35045c644ebbf18f500f7f83385908f
Sam Lee <leemingchun419@gmail.com>
2023-09-01 11:59:43 +0800

    update api route

--- src/constant/Url.tsx
+++ src/constant/Url.tsx
@@ -1,2 +1,3 @@
-export const DOMAIN = "https://localhost:44385/";
+export const DOMAIN = "https://git.heroku.com/twitter-clone-api-demo.git/";
+// export const DOMAIN = "https://localhost:44385/";
 // export const DOMAIN = "http://localhost:7021/";
